f24d6f171e846eb7d642f3657db3f09925fbfaa4
Unseeable8710 <78916578+Unseeable8710@users.noreply.github.com>
2025-06-22 15:55:27 -0400

    Added a license

--- src/jsvl.js
+++ src/jsvl.js
@@ -1,3 +1,14 @@
+// ==UserScript==
+// @name         JSVL
+// @namespace    Unseeable's Libraries
+// @version      1.0.0
+// @description  A simple tool that allows you to execute code when a global variable is located.
+// @author       Colton Stone
+// @homepage     https://github.com/Unseeable8710/JSVL
+// @license      GPL-3.0-or-later
+// @tag          utilities
+// ==/UserScript==
+
 function checkNested(obj, args) {
   for (var i = 0; i < args.length; i++) {
     if (!obj || !obj.hasOwnProperty(args[i])) {
